a02b7a68cb52eb678f83bc2217d8c1867e160e4a
Jorge Luiz Gomes <jluizgomes@gmail.com>
2016-05-02 00:36:26 -0300

    Adição de um preloaareload para a pagina de produtos.

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,9 @@
 /* global angular */
 
-var app = angular.module('peachApp', []);
+var app = angular.module('peachApp', ['angular-loading-bar']).config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
+    cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner">Custom Loading Message...</div>';
+  }])
 
 app.controller('listagem', ['$scope', '$http', function ($scope, $http) {
 	$http.get('http://peach-api.azurewebsites.net/api/Products/-23.84564988765934%20-47.434844970703125/100').success(function (data) {
