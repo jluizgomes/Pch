de827a42e444bea7eaf846a4315b190b7a9f3440
Jorge Luiz Gomes <jluizgomes@gmail.com>
2016-05-17 23:22:30 -0300

    Inclusão da rota de detalhe.

--- dist/app.js
+++ dist/app.js
@@ -14,10 +14,23 @@ app.controller('listagem', ['$scope', '$http', function($scope, $http) {
   });
 }]);
 
+app.controller('DetailCtrl', ['$scope', '$http', '$routeParams',
+  function($scope, $http, $routeParams) {
+    $scope.detailId = $routeParams.detailId;
+    // $http.get('http://peach-api.azurewebsites.net/api/Products/1').success(function(data) {
+    //   $scope.contents = data;
+    // });
+}]);
+
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
   when('/list', {
-    templateUrl: 'index.html'
+    templateUrl: 'index.html',
+    controller: 'listagem'
+  }).
+  when('/detail/:detailId', {
+    templateUrl: 'detail.html',
+    controller: 'DetailCtrl'
   }).
   otherwise({
     redirectTo: '/'
