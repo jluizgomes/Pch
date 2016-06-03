df2391f3d8653d988ecc21d75a76e1c7e629bc9e
Jorge Luiz Gomes <jluizgomes@gmail.com>
2016-06-02 22:37:02 -0300

    Ajuste na exibição do menu accordeon para todas as resoluçoes.

--- build/js/interface.js
+++ build/js/interface.js
@@ -42,52 +42,21 @@ $(function() {
     'height': $(window).height()
   });
 
-  if ($(window).width() > 992) {
-    $('.menu_items.side').show();
-    $('.menu_items.accordion').hide();
-    $('.submenu').removeClass('inside');
-    $('.submenu').addClass('outside');
-    $('.submenu').css({
-      'height': $(window).height() - 45
-    });
-
-    $('.menu_items ul').find('li').each(function() {
-      var $this = $(this);
-      $this.on('mouseenter', function() {
-        $this.find('a').addClass('active');
-        if($this.parent().has('.submenu')){
-          $this.find('.submenu').stop().fadeIn(300);
-          $this.find('i').css({
-            'color': '#b6422b'
-          })
-        } else {
-          return false;
-        }
-      }).on('mouseleave', function() {
-        $this.find('a').removeClass('active');
-        $('.submenu').stop().fadeOut(300);
-        $this.find('i').css({
-          'color': '#373737'
-        })
+  $('.menu_items.side').hide();
+  $('.menu_items.accordion').show();
+  $('.submenu').removeClass('outside');
+  $('.submenu').addClass('inside');
+  $('.menu_items').find('h3').on('click', function() {
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+      $('.menu_items h3').css({
+        'background': '#191919 url("./images/arrow-menu-list.png") 95% center no-repeat'
       });
-    });
-  } else {
-    $('.menu_items.side').hide();
-    $('.menu_items.accordion').show();
-    $('.submenu').removeClass('outside');
-    $('.submenu').addClass('inside');
-    $('.menu_items').find('h3').on('click', function() {
-      if ($(this).hasClass('active')) {
-        $(this).removeClass('active');
-        $('.menu_items h3').css({
-          'background': '#191919 url("./images/arrow-menu-list.png") 95% center no-repeat'
-        });
-      } else {
-        $(this).addClass('active');
-        $('.menu_items h3.active').css({
-          'background': '#020202 url("./images/arrow-menu-list-down-active.png") 95% center no-repeat'
-        });
-      }
-    });
-  }
+    } else {
+      $(this).addClass('active');
+      $('.menu_items h3.active').css({
+        'background': '#020202 url("./images/arrow-menu-list-down-active.png") 95% center no-repeat'
+      });
+    }
+  });
 });
